7705364120596cb20b75437fb61ed38e2e029e17
Udhay <udaybahuguna74@gmail.com>
2026-07-23 02:16:07 +0530

    Update fbrontend deployment config

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://studentmanagementsystem0011.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
