f2de513e73827898260f36cb914764c4ddbcdb0b
Udhay <udaybahuguna74@gmail.com>
2026-07-23 02:13:30 +0530

    Update frontend deployment config

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8001",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "VITE_API_BASE_URL",
 });
 
 api.interceptors.request.use((config) => {
